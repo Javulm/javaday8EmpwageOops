33c583bb6283c0991ad6f14ba917766d78969fca
Javul Mulla <javulmulla8@gmail.com>
2022-08-10 21:29:24 +0530

    Javul: Program refactored Using different classes, methods and variables

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,15 +1,73 @@
 package com.bridgelabz;
 
+class attendence{
+public final static int IS_PRESENT=1;
+public final static int IS_ABSENT=0;
+public final static int  IS_PART_TIME=2;
+public static final int SALARY_PER_HR = 20;
+protected static final int NO_OF_DAYS = 20;
+
+public static double check = Math.floor((Math.random() * 10) % 3);
+public static double check2;
+
+static void attendanceCheck() {
+	switch ((int) check) {
+	case IS_PRESENT:
+		System.out.println("employee is present.");
+		break;
+	case IS_PART_TIME:
+		System.out.println("employee is part time present.");
+	}
+}
+
+void partTimeCheck() {
+	if(check==IS_ABSENT) {
+		System.out.println("employ is absent.");
+	}
+}
+}
+class daywage extends attendence{
+	public static final int SALARY_PER_HR = 20;
+	static int working_hr;
+	static double salary;
+	static void wage() {
+		check2 = Math.floor((Math.random() * 10) % 3);
+		if (attendence.check2==attendence.IS_PRESENT) {
+			working_hr=8;
+		}
+		else if (attendence.check2==attendence.IS_PART_TIME) {
+			working_hr=4;
+		}
+		else {
+			working_hr=0;
+		}
+		salary = working_hr * SALARY_PER_HR;
+		System.out.println("One month Employee salary is: "+salary+"Rs");
+}
+}
+class totalwage extends daywage{
+	 void wage1() {
+		 double totalempwage=0.0;
+		for (int day=0; day<NO_OF_DAYS; day++) {
+			daywage.wage(); // accessing method from daywage class.
+			totalempwage = totalempwage + salary;
+			System.out.println("total one month employeee wage: " +totalempwage +"Rs");
+		}	
+	}
+}
 public class EmpWage {
 	 public static final int IS_FULL_TIME = 1;
 	 public static final int IS_PART_TIME = 2;
 	 public static final int SALARY_PER_HR = 20;
 	 public static final int NO_OF_DAYS = 20;
+	    public static final int MAX_HR_IN_MONTH = 100;
 	public static void employeeAttendace() {
 		int working_hr = 0;
-		int employeewage = 0;
-		int Totalempwage = 0;
-		for (int day = 0; day < NO_OF_DAYS; day++) {
+	    int totalEmpHrs = 0;
+       int totalEmpWorkingDay = 0;
+
+       while (totalEmpHrs <= MAX_HR_IN_MONTH && totalEmpWorkingDay < NO_OF_DAYS) {
+           totalEmpWorkingDay++;
 			double empCheck = Math.floor((Math.random() * 10) % 3);
 			switch ((int) empCheck) {
 			case IS_FULL_TIME:
@@ -24,14 +82,24 @@ public class EmpWage {
 				System.out.println("Employee is Absent");
 				working_hr = 0;
 			}
-			employeewage = working_hr * SALARY_PER_HR;
-			Totalempwage += employeewage;
-			System.out.println("Employee Wage :  " + employeewage);
+			  totalEmpHrs += working_hr;
+	            System.out.println("Day :  " + totalEmpWorkingDay + "   Emp Hr : " + working_hr);
 		}
-		System.out.println("Total Employee Wage : " + Totalempwage);
+       int totalEmpWage = totalEmpHrs * SALARY_PER_HR;
+		System.out.println("Total Employee Wage : " + totalEmpWage);
 	}
-
+	
 	public static void main(String[] args) {
-		EmpWage.employeeAttendace(); // main method
+		System.out.println("Welcome to employee wage computation.");
+//		EmpWage.employeeAttendace(); // main method programs ends here.
+		
+//same program using different class, methods and variables.
+		
+		attendence.attendanceCheck(); // static method accessed using class name
+		attendence obj=new attendence(); 
+		obj.partTimeCheck(); // accessed by creating object
+		daywage.wage();
+		totalwage obj2= new totalwage();
+		obj2.wage1();
 	}
 }
