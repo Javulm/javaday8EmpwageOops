30799c50dff1867d9284ab4497b1eeca9e5866c2
Javul Mulla <javulmulla8@gmail.com>
2022-08-15 09:48:57 +0530

    Modified: rearranged code.

--- src/com/bridgelabz/TotalWage.java
+++ src/com/bridgelabz/TotalWage.java
@@ -4,7 +4,7 @@ class TotalWage extends DayWage {
 		 void wage1() {
 			 double totalempwage=0.0;
 			for (int day=0; day<NO_OF_DAYS; day++) {
-				DayWage.wage(); // accessing method from daywage class.
+				DayWage.wage(); // accessing method from DayWage class.
 				totalempwage = totalempwage + salary;
 				System.out.println("total one month employeee wage: " +totalempwage +"Rs");
 			}	
