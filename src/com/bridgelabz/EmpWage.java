2c422f12a0e15438a707aa4ebb197e4784f93e4b
Javul Mulla <javulmulla8@gmail.com>
2022-08-15 09:56:50 +0530

    Modified: Added two new files for different classes

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,40 +1,23 @@
 package com.bridgelabz;
 
 public class EmpWage {
-	 public static final int IS_FULL_TIME = 1;
-	 public static final int IS_PART_TIME = 2;
-	 public static final int SALARY_PER_HR = 20;
-	 public static final int NO_OF_DAYS = 20;
-	    public static final int MAX_HR_IN_MONTH = 100;
-	public static void employeeAttendace() {
-		int working_hr = 0;
-	    int totalEmpHrs = 0;
-        int totalEmpWorkingDay = 0;
-
-        while (totalEmpHrs <= MAX_HR_IN_MONTH && totalEmpWorkingDay < NO_OF_DAYS) {
-            totalEmpWorkingDay++;
-			double empCheck = Math.floor((Math.random() * 10) % 3);
-			switch ((int) empCheck) {
-			case IS_FULL_TIME:
-				System.out.println("Employee is Present");
-				working_hr = 8;
-				break;
-			case IS_PART_TIME:
-				System.out.println("Employee is part time Present");
-				working_hr = 4;
-				break;
-			default:
-				System.out.println("Employee is Absent");
-				working_hr = 0;
-			}
-			  totalEmpHrs += working_hr;
-	            System.out.println("Day :  " + totalEmpWorkingDay + "   Emp Hr : " + working_hr);
-		}
-        int totalEmpWage = totalEmpHrs * SALARY_PER_HR;
-		System.out.println("Total Employee Wage : " + totalEmpWage);
-	}
+	final static int IS_PRESENT = 1;
+	final static int IS_ABSENT = 0;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int SALARY_PER_HR = 20;
+	public static final int NO_OF_DAYS = 20;
+	public static final int MAX_HR_IN_MONTH = 100;
+	public static double check = Math.floor((Math.random() * 10) % 3);
+	public static double check2;
 
 	public static void main(String[] args) {
-		EmpWage.employeeAttendace(); // main method
+		System.out.println("Welcome to employee wage computation.");
+		DayWage.attendanceCheck(); // static method accessed using class name
+		DayWage part = new DayWage();
+		part.partTimeCheck(); // accessed by creating object
+		DayWage.wage();
+		TotalWage obj2 = new TotalWage();
+		obj2.wage1();
 	}
 }
